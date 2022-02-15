816b98f2c31f63afccbe65446f261b4ec7e87a46
Dhirendra Thakur <drthunk@gmail.com>
2022-02-15 14:34:47 +0545

    midupdated

---
+++ src/mycomponents/mid.js
@@ -0,0 +1,40 @@
+import { Component } from "react";
+
+//import Home from "./home";
+import Register  from "./register";
+
+
+import Login from "./login";
+import AddSupplement from "./AddSupplement";
+//import updateSupplement from "./updateSupplement";
+import UpdateSupplement from "./updateSupplement";
+import Showsupp from "./showSupplement";
+import Addproduct from "./Addproduct";
+import ShowProduct from "./showproduct";
+//import Home from "./home";
+
+//import {Route, Routes} from 'react-router-dom';
+import {Route, Routes } from "react-router-dom";
+
+class Mid extends Component{
+    render(){
+        return(
+            <div>
+                <Routes>
+               
+                <Route path='customer/register' element={<Register/>} />
+                <Route path='/about' element={<about/>} />
+                <Route path ='customer/login' element ={<Login/>}/>
+                <Route path= '/Addsupplement' element={<AddSupplement/>}/>
+                <Route path ='/updatesupplement/:sid' element={<UpdateSupplement/>}/>
+                <Route path ='/showsupplement' element ={<Showsupp/>}/>   
+                <Route path ='/addproduct' element={<Addproduct/>}/>   
+                <Route path="/showproduct" element={<ShowProduct/>}/>
+                </Routes>
+
+            </div>
+
+        )
+    }
+}
+export default Mid;
