16475a4c7d1f37c7c28dc33246cf741fa1dbf4b0
Dhirendra Thakur <drthunk@gmail.com>
2022-02-15 14:34:23 +0545

    homeupdated

---
+++ src/mycomponents/home.js
@@ -0,0 +1,61 @@
+//import { MoneyOffCsredRounded } from "@mui/icons-material";
+import { Component } from "react";
+//import { Collapse } from "react-bootstrap";
+
+
+
+//import {IconButton} from '@material-ui/core';
+//import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+class Home extends Component{
+    
+    render(){
+        
+        return(
+                
+            <div className="backimage">
+                <h1 className="hometop">Welcome to <br/>our <br/>Fitness page.</h1>
+            <div></div>
+
+
+            <div className="container"> 
+             {/* ////Code for card image  */}
+            <div className="e-card">
+            <div className="e-card-image">
+            <div className="e-card-title">Product Brif</div>
+            <h1>Hello WelCome to the Fitness website</h1>
+        </div>
+
+
+        {/* git push -u origin main */}
+           
+            </div> 
+
+
+
+
+
+
+
+            <div className="col-md-2" >   
+            <div className="e-card">
+            <div className="e-card-image">
+            <div className="e-card-title">Product Brif</div>
+            <h1>Hello WelCome to the Fitness website</h1>
+        </div>
+
+
+           
+            </div> 
+            
+            </div>
+            </div>
+           
+            </div>
+
+           
+           
+            
+        )
+    }
+}
+export default Home;
