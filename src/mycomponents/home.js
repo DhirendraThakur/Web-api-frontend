f09710eef5f9fbb6faa9e3e1b72e188abed2336d
Dhirendra Thakur <drthunk@gmail.com>
2022-02-16 15:53:29 +0545

    designupdated

--- src/mycomponents/home.js
+++ src/mycomponents/home.js
@@ -1,8 +1,12 @@
 //import { MoneyOffCsredRounded } from "@mui/icons-material";
 import { Component } from "react";
 //import { Collapse } from "react-bootstrap";
+import { Card, Button } from "react-bootstrap";
+import Image from 'react-bootstrap/Image';
+{/* git push -u origin main */}
+import SimpleImageSlider from "react-simple-image-slider";
 
-
+           
 
 //import {IconButton} from '@material-ui/core';
 //import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
@@ -14,43 +18,72 @@ class Home extends Component{
                 
             <div className="backimage">
                 <h1 className="hometop">Welcome to <br/>our <br/>Fitness page.</h1>
-            <div></div>
+            
 
 
-            <div className="container"> 
-             {/* ////Code for card image  */}
-            <div className="e-card">
-            <div className="e-card-image">
-            <div className="e-card-title">Product Brif</div>
-            <h1>Hello WelCome to the Fitness website</h1>
-        </div>
 
 
-        {/* git push -u origin main */}
+{/* 
+           <div className=" homesecond">               
+                <div className="row">
+                    <p>asdasda</p>
+                </div>
+           </div> */}
+<div className="row">
+<Card  style={{ width: '18rem' }} className="homesecond">
+  <Card.Img variant="top"src="image/panda.jpg"/>
+  <Card.Body >
+    <Card.Title>Supplement</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
+
+
+<Card  style={{ width: '18rem' }} className="homethird">
+  <Card.Img variant="top"src="image/panda.jpg"/>
+  <Card.Body >
+    <Card.Title>Supplement</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
+
+<Card  style={{ width: '18rem' }} className="homethird">
+  <Card.Img variant="top"src="image/panda.jpg"/>
+  <Card.Body >
+    <Card.Title>Supplement</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
+        
+</div>
+
+
+
+
+
+
+
+
+
            
-            </div> 
-
-
-
-
-
-
-
-            <div className="col-md-2" >   
-            <div className="e-card">
-            <div className="e-card-image">
-            <div className="e-card-title">Product Brif</div>
-            <h1>Hello WelCome to the Fitness website</h1>
-        </div>
-
-
-           
-            </div> 
+            
             
             </div>
-            </div>
            
-            </div>
+           
+            
 
            
            
