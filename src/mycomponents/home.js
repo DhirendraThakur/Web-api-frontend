2c4050b1bce6e66f6c7e15c8dbdf9be84f3f1819
Dhirendra Thakur <drthunk@gmail.com>
2022-02-17 15:35:58 +0545

    homepageupdated

--- src/mycomponents/home.js
+++ src/mycomponents/home.js
@@ -1,94 +1,127 @@
-//import { MoneyOffCsredRounded } from "@mui/icons-material";
-import { Component } from "react";
-//import { Collapse } from "react-bootstrap";
+//import { Component } from "react";
 import { Card, Button } from "react-bootstrap";
-import Image from 'react-bootstrap/Image';
+import React, {useState} from 'react';
+import dataSlider from './dataslider';
+import BtnSlider from "./btnslider";
+
+
 {/* git push -u origin main */}
-//import SimpleImageSlider from "react-simple-image-slider";
-
-           
-
-//import {IconButton} from '@material-ui/core';
-//import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-class Home extends Component{
-    
-    render(){
-        
-        return(
-                
-            <div className="backimage">
-                <h1 className="hometop">Welcome to <br/>our <br/>Fitness page.</h1>
-            
 
 
 
+const Home =()=> {
 
-{/* 
-           <div className=" homesecond">               
-                <div className="row">
-                    <p>asdasda</p>
-                </div>
-           </div> */}
-<div className="row">
-<Card  style={{ width: '18rem' }} className="homesecond">
-  <Card.Img variant="top"src="image/panda.jpg"/>
-  <Card.Body >
-    <Card.Title>Supplement</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
+    const [slideIndex, setSlideIndex] = useState(1)
 
-
-<Card  style={{ width: '18rem' }} className="homethird">
-  <Card.Img variant="top"src="image/panda.jpg"/>
-  <Card.Body >
-    <Card.Title>Supplement</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
-
-<Card  style={{ width: '18rem' }} className="homethird">
-  <Card.Img variant="top"src="image/panda.jpg"/>
-  <Card.Body >
-    <Card.Title>Supplement</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
-        
-</div>
-
-
-
-
-
-
-
-
-
-           
-            
-            
-            </div>
-           
-           
-            
-
-           
-           
-            
-        )
+    const nextSlide = () => {
+        if(slideIndex !== dataSlider.length){
+            setSlideIndex(slideIndex + 1)
+        } 
+        else if (slideIndex === dataSlider.length){
+            setSlideIndex(1)
+        }
     }
-}
-export default Home;
+
+    const prevSlide = () => {
+        if(slideIndex !== 1){
+            setSlideIndex(slideIndex - 1)
+        }
+        else if (slideIndex === 1){
+            setSlideIndex(dataSlider.length)
+        }
+    }
+
+    const moveDot = index => {
+        setSlideIndex(index)
+    }
+
+
+    return (
+
+        <div className="backimage">
+                         <h1 className="hometop">Welcome to <br/>our <br/>Fitness page.</h1>
+        
+         <div className="row">
+         <Card  style={{ width: '18rem' }} className="homesecond">
+           <Card.Img variant="top"src="https://i.etsystatic.com/19146535/r/il/983380/2134356202/il_fullxfull.2134356202_hij3.jpg"/>
+           <Card.Body >
+             <Card.Title>Supplement</Card.Title>
+             <Card.Text>
+               Some quick example text to build on the card title and make up the bulk of
+               the card's content.
+             </Card.Text>
+             <Button variant="primary">Go somewhere</Button>
+           </Card.Body>
+         </Card>
+        
+        
+         <Card  style={{ width: '18rem' }} className="homethird">
+           <Card.Img variant="top"src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReTuJs72vzfRTp5lRLFj9GAgROfBWcc1vi3sPhM9p8D_0ZvIJrXwzRfU8J-V8Hd0Z6VTs&usqp=CAU"/>
+           <Card.Body >
+             <Card.Title>Product</Card.Title>
+             <Card.Text>
+               Some quick example text to build on the card title and make up the bulk of
+               the card's content.
+             </Card.Text>
+             <Button variant="primary">Go somewhere</Button>
+           </Card.Body>
+         </Card>
+        
+         <Card  style={{ width: '18rem' }} className="homethird">
+           <Card.Img variant="top"src="https://rukminim1.flixcart.com/image/416/416/poster/x/f/w/abstract-cartoon-body-builder-psu000003283-small-original-imaekd9hvzgchpmb.jpeg?q=70"/>
+          <Card.Body >
+             <Card.Title>Workout</Card.Title>
+             <Card.Text>
+               Some quick example text to build on the card title and make up the bulk of
+               the card's content.
+             </Card.Text>
+             <Button variant="primary">Go somewhere</Button>
+           </Card.Body>
+         </Card>
+                
+         </div>
+        
+
+
+        <div className="container-slider">
+            {dataSlider.map((obj, index) => {
+                return (
+                    <div
+                    key={obj.id}
+                    className={slideIndex === index + 1 ? "slide active-anim" : "slide"}
+                    >
+                        {/* <img 
+                        src={process.env.PUBLIC_URL + `./Imgs/img ${index + 1}.jpg`} 
+                        /> */}
+
+<img src={process.env.PUBLIC_URL + `/Imgs/panda${index + 1}.jpg`}
+
+                        />
+
+                    </div>
+                )
+            })}
+            <BtnSlider moveSlide={nextSlide} direction={"next"} />
+            <BtnSlider moveSlide={prevSlide} direction={"prev"}/>
+
+            <div className="container-dots">
+                {Array.from({length: 5}).map((item, index) => (
+                    <div 
+                    onClick={() => moveDot(index + 1)}
+                    className={slideIndex === index + 1 ? "dot active" : "dot"}
+                    ></div>
+                ))}
+            </div>
+        </div>
+   
+    
+
+
+
+
+
+</div>
+    
+        )    }  
+
+export default Home
